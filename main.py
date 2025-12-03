780b38bb3579df8eb37f90e6ee8f5509f417c5d3
tileotter <adamfoster913@gmail.com>
2025-12-03 13:12:11 -0500

    Basic AI agent functionality and printed output including prompt, token counts, and response.

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("No API key!")
+    
+    client = genai.Client(api_key=api_key)
+
+    prompt="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=prompt
+    )
+
+    if response.usage_metadata == None:
+        raise RuntimeError("Failed API request!")
+    
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
+
+if __name__ == "__main__":
+    main()

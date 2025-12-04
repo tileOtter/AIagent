525ee1d1bef94ec8ce7f177cc590602e27dca314
tileotter <adamfoster913@gmail.com>
2025-12-04 16:58:31 -0500

    Prepared code for AI agent to remember history, and added verbose argument (includes prompt and token info in response when True.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -14,22 +15,25 @@ def main():
     client = genai.Client(api_key=api_key)
 
     parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("prompt", type=str, help="User prompt")
+    parser.add_argument("prompt", type=str, help="User prompt") 
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
     prompt = args.prompt
 
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=prompt
+        contents=messages
     )
 
     if response.usage_metadata == None:
         raise RuntimeError("Failed API request!")
     
-    print(f"User prompt: {prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose is True:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":

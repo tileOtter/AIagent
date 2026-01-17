1e3e9335271b0e83e6c9d719be7b0f76dc437ac0
tileOtter <adamfoster913@gmail.com>
2026-01-17 16:20:49 -0500

    Updated code so agent can utilize functions, remember previous actions and act accordingly.

--- main.py
+++ main.py
@@ -24,35 +24,42 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.prompt)])]
     prompt = args.prompt
 
-    response = client.models.generate_content(
+    for _ in range(20):
+        response = client.models.generate_content(
         model='gemini-2.5-flash', 
         contents=messages,
         config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0)
-    )
+        )
 
-    if response.usage_metadata == None:
-        raise RuntimeError("Failed API request!")
-    
-    if args.verbose is True:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.candidates != None:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    if response.function_calls != None:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call)
-            if function_call_result.parts == None:
-                raise Exception
-            if function_call_result.parts[0].function_response == None:
-                raise Exception
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception
-            function_result_list = []
-            function_result_list.append(function_call_result.parts[0])
+        if response.usage_metadata == None:
+            raise RuntimeError("Failed API request!")
+        
         if args.verbose is True:
-            print(f"-> {function_call_result.parts[0].function_response.response["result"]}")    
-    else:
-        print(f"Response:\n{response.text}")
+            print(f"User prompt: {prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+        if response.function_calls != None:
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call)
+                if function_call_result.parts == None:
+                    raise Exception
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception
+                function_result_list = []
+                function_result_list.append(function_call_result.parts[0])
+            messages.append(types.Content(role="user", parts=function_result_list))
+            if args.verbose is True:
+                print(f"-> {function_call_result.parts[0].function_response.response["result"]}")    
+        else:
+            print(f"Response:\n{response.text}")
+            return
+       
 if __name__ == "__main__":
     main()
